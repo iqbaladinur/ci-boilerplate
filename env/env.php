23f928f835ea390f30adfd0b794f1baffc747012
Iqbal Adi Nurmansyah <iqbaladinur@gmail.com>
2019-09-04 13:09:09 +0700

    update ci version

--- env/env.php
+++ env/env.php
@@ -3,5 +3,5 @@
 	 * this variable set your local environment folder to decide production or development environment.
 	 * it will bi used in the public index.php
 	 * */
-	$localProjectLocation = "D:\php\boilerplate-ci";
+	$localProjectLocation = "E:\Open Source\ci-boilerplate";
 ?>
